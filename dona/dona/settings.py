51db61fda42477ab845fda943132b70e20f9a432
NessX4 <0322103677@ut-tijuana.edu.mx>
2025-07-09 18:39:27 -0700

    gitignore modified

--- dona/dona/settings.py
+++ dona/dona/settings.py
@@ -84,8 +84,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'dona_app_django',  # Updated to match the new database name
-        "USER": "luna",
-        "PASSWORD": "luna18",
+        "USER": "postgres",
+        "PASSWORD": "2305",
         "HOST": "localhost",
         "PORT": "5432",
 
