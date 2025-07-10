aa400532813ac8a560b8d32e1172db7c0a6b29fb
NessX4 <0322103677@ut-tijuana.edu.mx>
2025-07-09 19:30:53 -0700

    Descripción clara del cambio

--- dona/home/urls.py
+++ dona/home/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.landing_page, name='landing'),
+    path('landing', views.landing_pageone, name='landing'),
     path('logout/', views.logout_view, name='logout'),
     path('loggin/', views.login_view, name='loggin'),
     path('registro/', views.registro_view, name='registro'),
     path('MisDonaciones/', views.donaciones_view, name='Donaciones'),
-    path('NuevaContraseña/', views.NuevaContra_view, name='Ncontra'),
     path('Perfil/', views.perfil_view, name='perfil'),
     path('Notificaciones/', views.Notificaciones_view, name='notificaciones'),
     path('PublicarDonacion/', views.publicacdonacion_view, name='publicardoonacion'),
