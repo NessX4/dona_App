2d501a6488749c534835eb5f22ea5913534138d0
NessX4 <0322103677@ut-tijuana.edu.mx>
2025-07-22 19:38:02 -0700

    registro de usuarios/login

--- dona/usuarios/urls.py
+++ dona/usuarios/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import (
-    RolViewSet, UsuarioViewSet, DonadorViewSet, ReceptorViewSet,
-    VoluntarioViewSet, AdministradorViewSet
+    UsuarioViewSet, RolViewSet,
+    DonadorViewSet, ReceptorViewSet,
+    VoluntarioViewSet, AdministradorViewSet,
+    login_view
 )
 
 router = DefaultRouter()
-router.register(r'roles', RolViewSet)
 router.register(r'usuarios', UsuarioViewSet)
+router.register(r'roles', RolViewSet)
 router.register(r'donadores', DonadorViewSet)
 router.register(r'receptores', ReceptorViewSet)
 router.register(r'voluntarios', VoluntarioViewSet)
@@ -15,4 +17,5 @@ router.register(r'administradores', AdministradorViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('login/', login_view, name='login')
 ]
