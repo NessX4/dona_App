3977fd65bb19f0347e9fdb6ec9ed15b7c8a29c63
Mariela Higuera <0322103734@ut-tijuana.edu.mx>
2025-07-23 16:26:11 -0700

    Arreglado problema de superusuarios, se ven en admin tambien

--- dona/usuarios/signals.py
+++ dona/usuarios/signals.py
@@ -1,26 +1,16 @@
 from django.db.models.signals import post_save
-from django.contrib.auth.models import User
 from django.dispatch import receiver
-from usuarios.models import Usuario, Administrador  # Ajusta el import según tu estructura
+from django.contrib.auth.models import User
+from usuarios.models import Administrador
 
 @receiver(post_save, sender=User)
-def crear_administrador_superusuario(sender, instance, created, **kwargs):
+def crear_administrador_si_superusuario(sender, instance, created, **kwargs):
     if created and instance.is_superuser:
-        # Verifica si ya existe el Usuario
-        if not Usuario.objects.filter(correo=instance.email).exists():
-            usuario = Usuario.objects.create(
-                nombre=instance.username,
-                correo=instance.email,
-                contraseña='',
-                rol_id=4,  # Ajusta al ID correcto del rol "Administrador"
-                activo=True
-            )
-
-            # Crea el administrador vinculado al nuevo Usuario
+        if not Administrador.objects.filter(correo=instance.email).exists():
             Administrador.objects.create(
-                usuario=usuario,
                 nombre=instance.username,
                 correo=instance.email,
-                contraseña='',
+                contraseña='importado',
                 activo=True
             )
+            print(f"...........................................")
