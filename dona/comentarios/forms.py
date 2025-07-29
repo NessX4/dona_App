62c68eebf4dff90b1546567eb88323a5412f3438
NessX4 <0322103677@ut-tijuana.edu.mx>
2025-07-29 14:59:59 -0700

    comentarios app comentarios

--- dona/comentarios/forms.py
+++ dona/comentarios/forms.py
@@ -1,12 +1,15 @@
+# Vanessa Balderas Martinez
 from django import forms
 from .models import Comentario
 
+# Formulario para crear o editar comentarios
 class ComentarioForm(forms.ModelForm):
     class Meta:
-        model = Comentario
-        fields = ['publicacion', 'usuario', 'comentario']
+        model = Comentario  # Modelo asociado al formulario
+        fields = ['publicacion', 'usuario', 'comentario']  # Campos que se mostrarán en el formulario
         widgets = {
-            'comentario': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
-            'publicacion': forms.Select(attrs={'class': 'form-control'}),
-            'usuario': forms.Select(attrs={'class': 'form-control'}),
+            'comentario': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),  # Área de texto para el comentario
+            'publicacion': forms.Select(attrs={'class': 'form-control'}),  # Selector para la publicación
+            'usuario': forms.Select(attrs={'class': 'form-control'}),  # Selector para el usuario
         }
+        
