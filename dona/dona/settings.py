a2231fe1c6979e6a0d0fcf121df992305fcaec69
NessX4 <0322103677@ut-tijuana.edu.mx>
2025-07-29 21:58:54 -0700

    version sin error del login

--- dona/dona/settings.py
+++ dona/dona/settings.py
@@ -102,7 +102,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'dona_app_django',  # Updated to match the new database name
         "USER": "postgres",
-        "PASSWORD": "Chavezca2!",
+        "PASSWORD": "2305",
         "HOST": "localhost",
         "PORT": "5432",
 
